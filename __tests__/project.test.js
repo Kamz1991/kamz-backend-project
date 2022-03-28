18ac05f3f4b41714d5c40adb9b0e45a18285a89e
Kamz1991 <89602011+Kamz1991@users.noreply.github.com>
2022-03-28 16:29:11 +0100

    get topics

---
+++ __tests__/project.test.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const db = require("../db/connection");
+const app = require("../db/app");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => seed(testData));
+
+afterAll(() => db.end());
+
+describe("1. GET /api/topics", () => {
+  test("status:200, responds with an array of topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const { topics } = body;
+        expect(topics).toBeInstanceOf(Array);
+        topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              description: expect.any(String),
+              slug: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
