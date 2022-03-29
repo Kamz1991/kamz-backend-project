6a92b0233fb6486c692623c00f696da891b4378a
Kamz1991 <89602011+Kamz1991@users.noreply.github.com>
2022-03-29 12:45:35 +0100

    complete get article id and error

--- db/app.js
+++ db/app.js
@@ -1,4 +1,4 @@
-const { getTopics } = require("../controllers/controller");
+const { getTopics, getAuthorById } = require("../controllers/controller");
 const express = require("express");
 
 const app = express();
@@ -7,8 +7,32 @@ app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getAuthorById);
+
+app.use((err, req, res, next) => {
+  const badReqCodes = ["42703", "22P02"];
+  if (badReqCodes.includes(err.code)) {
+    res.status(400).send({ msg: "bad request" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
 app.all("/*", (req, res) => {
-  res.status(404).send({ msg: "Route not found" });
+  res.status(404).send({ msg: "path not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err, "<<< err");
+  res.status(500).send({ msg: "internal server error" });
 });
 
 module.exports = app;
