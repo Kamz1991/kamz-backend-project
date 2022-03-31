689561186a1d6ff306e9eaec4d2d437775ba51c5
Kamz1991 <89602011+Kamz1991@users.noreply.github.com>
2022-03-31 12:40:10 +0100

    completed comment count

--- models/GetArticleById.model.js
+++ models/GetArticleById.model.js
@@ -1,9 +1,16 @@
 const db = require("../db/connection");
 exports.selectArticleById = async (article_id) => {
   const result = await db.query(
-    `SELECT * FROM articles WHERE article_id = $1;`,
+    `SELECT articles.*, 
+    COUNT(comment_id) AS comment_count
+    FROM articles
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id
+    LIMIT 1;`,
     [article_id]
   );
+
   if (result.rows.length === 0) {
     return Promise.reject({ status: 404, msg: "article not found" });
   } else {
