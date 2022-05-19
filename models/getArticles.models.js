a0df6b0071db813f9d70c5d1a6bbe5ffa04750e8
Kamz1991 <89602011+Kamz1991@users.noreply.github.com>
2022-05-19 17:55:24 +0100

    complete back end

--- models/getArticles.models.js
+++ models/getArticles.models.js
@@ -29,35 +29,3 @@ exports.selectArticles = async (query) => {
 
   return result.rows;
 };
-// exports.selectArticles = async (
-//   sort_by = "created_at",
-//   order = "desc",
-//   topic
-// ) => {
-//   const topics = await selectTopics();
-
-//   topicsArray = topics.map((object) => object.slug);
-//   if (topic !== undefined && !topicsArray.includes(topic)) {
-//     return Promise.reject({ status: 404, message: "topic not found" });
-//   }
-
-//   if (!/^desc$/i.test(order) && !/^asc$/i.test(order)) {
-//     return Promise.reject({ status: 400, message: "bad request" });
-//   }
-//   const queryValues = [];
-//   let queryStr = `SELECT articles.*, COUNT(comment_id) AS comment_count
-//   FROM articles
-//   JOIN comments
-//   ON articles.article_id = comments.article_id`;
-
-//   if (topic) {
-//     queryStr += ` WHERE topic = $1`;
-//     queryValues.push(topic);
-//   }
-
-//   queryStr += ` GROUP BY articles.article_id
-//   ORDER BY ${sort_by} ${order};`;
-
-//   const results = await db.query(queryStr, queryValues);
-//   return results.rows;
-// };
